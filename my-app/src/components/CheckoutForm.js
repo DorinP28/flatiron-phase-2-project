b7dfb7eca26b3c3a7ccaacfc98a4402b1ef7394f
Dorin Porcereanu <porcereanu.dorin@gmail.com>
2023-07-23 17:07:25 -0400

    update ProductCard.js

---
+++ my-app/src/components/CheckoutForm.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+
+const CheckoutForm = ({ navigateTo }) => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    address: "",
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Send the form data to the server
+    const configObj = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formData),
+    };
+
+    fetch("http://localhost:3000/orders", configObj)
+      .then((res) => res.json())
+      .then((data) => {
+        // Assuming the server returns the newly created order object with an ID
+        console.log("Order submitted successfully:", data);
+        // Clear the form and navigate back to the home page
+        setFormData({ name: "", email: "", address: "" });
+        navigateTo("/");
+      })
+      .catch((error) => {
+        console.error("Error submitting order:", error);
+        // Handle any errors or show error messages to the user
+      });
+  };
+
+  return (
+    <form className="checkout-form" onSubmit={handleSubmit}>
+      <input type="text" name="name" value={formData.name} onChange={handleInputChange} placeholder="Name" required />
+      <input type="email" name="email" value={formData.email} onChange={handleInputChange} placeholder="Email" required />
+      <input type="text" name="address" value={formData.address} onChange={handleInputChange} placeholder="Address" required />
+      <button type="submit">Place Order</button>
+    </form>
+  );
+};
+
+export default CheckoutForm;
