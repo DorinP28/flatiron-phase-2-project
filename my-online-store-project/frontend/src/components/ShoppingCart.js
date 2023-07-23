b79697697a82b05a2f0c46d4023831a12b2cf09c
Dorin Porcereanu <porcereanu.dorin@gmail.com>
2023-07-22 22:41:32 -0400

    ShoppingCard.js update

---
+++ my-online-store-project/frontend/src/components/ShoppingCart.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ShoppingCart() {
+  // Add code to fetch and display items from the shopping cart (if implemented)
+  return (
+    <div>
+      <h2>Shopping Cart</h2>
+      {/* Display items in the shopping cart */}
+    </div>
+  );
+}
+
+export default ShoppingCart;
