49dfcd6ad1a5260fd275715303ef6575010132a9
Dorin Porcereanu <porcereanu.dorin@gmail.com>
2023-07-23 23:11:10 -0400

    change about page and home page, add description in the readme.md

--- my-app/src/components/AboutPage.js
+++ my-app/src/components/AboutPage.js
@@ -1,10 +1,24 @@
 import React from "react";
+import "../styles.css";
 
 const AboutPage = () => {
   return (
-    <div>
+    <div className="about-page">
       <h2>About Our Store</h2>
-      <p>Will update shortly</p>
+      <p>
+        Welcome to DevTees, your one-stop-shop for the most creative and quirky developer T-shirts! We understand that as software developers, you're not just
+        about codes and coffee. You have a unique sense of humor that deserves to be shared and appreciated.
+      </p>
+      <p>
+        Our T-shirts are a perfect blend of comfort, style and a dash of humor. Each design is carefully chosen to resonate with your life as a developer - from
+        the joy of solving complex problems, to the delight of finding the perfect algorithm, and the occasional frustrations of debugging that piece of code.
+      </p>
+      <h3>Our Mission</h3>
+      <p>
+        At DevTees, our mission is to give software developers a fun and stylish way to express their unique perspective. We believe in celebrating the wit,
+        wisdom, and whimsy of the developer life. Our T-shirts are not just about making a fashion statement, but also about building a community that
+        appreciates the lighter side of programming.
+      </p>
     </div>
   );
 };
--- my-app/src/components/HomePage.js
+++ my-app/src/components/HomePage.js
@@ -1,10 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "../styles.css";
 
 const HomePage = () => {
   return (
     <div>
-      <h2>Welcome to the Home Page</h2>
-      <p>Will update shortly</p>
+      <div className="hero">
+        <h1>Welcome to Our Online Store!</h1>
+        <p>Quality products at the best prices</p>
+        <Link to="/products" className="cta">
+          Shop Now
+        </Link>
+      </div>
+
+      <div className="intro">
+        <h2>About Us</h2>
+        <p>
+          At Our Online Store, we strive to offer our customers the best products at unbeatable prices. Our collection is carefully selected to ensure top
+          quality and satisfaction. Start shopping today!
+        </p>
+      </div>
     </div>
   );
 };
